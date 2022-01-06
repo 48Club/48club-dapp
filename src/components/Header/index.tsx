3359b681464b98162ec008deaab4a67be2b0cdb6
callmewhy <whywanghai@gmail.com>
2022-01-06 21:00:33 +0800

    no message

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import appLogo from '../../assets/images/icon/logo-app.svg'
 import { useTranslation } from 'react-i18next'
@@ -6,17 +6,25 @@ import i18n from 'i18next'
 import MobileModal from './MobileModal'
 import { CloseOutlined, MenuOutlined } from '@ant-design/icons'
 import { Dropdown, Menu } from 'antd'
+import { useEthers } from '@usedapp/core'
+import { shorten } from '@funcblock/dapp-sdk'
 
 export default function Header() {
   const { t } = useTranslation()
+  const { activateBrowserWallet, error, account } = useEthers()
   const [language, setLanguage] = React.useState(localStorage.getItem('language') || 'en')
-  React.useEffect(() => {
+
+  useEffect(() => {
     localStorage.setItem('language', language)
-    i18n.changeLanguage(language)
+    i18n.changeLanguage(language).catch(console.error)
   }, [language])
 
+  const activate = useCallback(async () => {
+    await activateBrowserWallet()
+  }, [activateBrowserWallet])
+
   const menu = (
-    <Menu className='bg-white'>
+    <Menu className="bg-white">
       <Menu.Item>
         <Link className="text-black opacity-75 hover:text-primary" to={'/'}>{t('app_header_menu_home_title')}</Link>
       </Menu.Item>
@@ -36,14 +44,14 @@ export default function Header() {
                ? 'https://drive.google.com/file/d/1RZxJamk3dK2w-4e4TI98uXe1bCLd2Vz1/view?usp=sharing'
                : 'https://drive.google.com/file/d/1NFZiOoILO59LemFn2-_LHMD7c3nDBHla/view?usp=sharing'
            }
-           target='_blank'
-           rel='noopener noreferrer'
+           target="_blank"
+           rel="noopener noreferrer"
         >
           {t('app_header_menu_whitepaper_title')}
         </a>
       </Menu.Item>
     </Menu>
-  );
+  )
 
   const [showResponsiveMenu, setShowResponsiveMenu] = React.useState(false)
   return (
@@ -53,23 +61,25 @@ export default function Header() {
           <Link to={'/'}>
             <img alt={''} src={appLogo} className="h-3 md:h-5 mr-3" />
           </Link>
-          <div className='hidden md:flex flex-row items-center'>
+          <div className="hidden md:flex flex-row items-center">
             <Link className="text-black ml-5 font-medium hover:text-primary" to={'/staking'}>{t('staking')}</Link>
             <Link className="text-black ml-5 font-medium hover:text-primary" to={'/nft'}>NFT</Link>
             <Link className="text-black ml-5 font-medium hover:text-primary" to={'/voting'}>{t('voting')}</Link>
-            <Dropdown overlay={menu} placement="bottomLeft" overlayStyle={{zIndex: 10000}}>
+            <Dropdown overlay={menu} placement="bottomLeft" overlayStyle={{ zIndex: 10000 }}>
               <div className="text-black ml-5 font-medium hover:text-primary cursor-pointer">More</div>
             </Dropdown>
           </div>
         </div>
         <div className="flex flex-row items-center">
-          <div className='bg-secondary flex flex-row items-center cursor-pointer'>
+          <div className="bg-secondary flex flex-row items-center cursor-pointer">
             <div className="text-primary px-3">BSC</div>
-            <div className="px-4 bg-primary rounded py-2">{t('connect_wallet')}</div>
+            <div className="px-4 bg-primary rounded py-2" onClick={activate}>
+              {account ? shorten(account) : t('connect_wallet')}
+            </div>
           </div>
           <div className="ml-4 hidden md:flex flex-row text-xs font-semibold">
-            <div className="cursor-pointer opacity-75 hover:text-primary" onClick={() => setLanguage('cn')} style={{color: language === 'cn' ? '#FFC801' : 'black' }}>中文</div>
-            <div className="cursor-pointer opacity-75 hover:text-primary ml-2 pl-2 border-l" onClick={() => setLanguage('en')} style={{color: language === 'en' ? '#FFC801' : 'black' }}>EN</div>
+            <div className="cursor-pointer opacity-75 hover:text-primary" onClick={() => setLanguage('cn')} style={{ color: language === 'cn' ? '#FFC801' : 'black' }}>中文</div>
+            <div className="cursor-pointer opacity-75 hover:text-primary ml-2 pl-2 border-l" onClick={() => setLanguage('en')} style={{ color: language === 'en' ? '#FFC801' : 'black' }}>EN</div>
           </div>
         </div>
         <div className="block md:hidden">
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
 import React, { Suspense } from 'react'
 import { Provider as ReduxProvider } from 'react-redux'
 import ReactDOM from 'react-dom'
-import { ChainId, DAppProvider } from '@usedapp/core'
+import { BSCTestnet, Config, DAppProvider } from '@usedapp/core'
 import './assets/styles/antd.less'
 import './assets/styles/index.css'
 import App from './App'
 import store from './state'
-import { Config } from '@usedapp/core/src/model/config/Config'
 
 const config: Partial<Config> = {
-  readOnlyChainId: ChainId.BSCTestnet,
+  networks: [BSCTestnet],
 }
 
 ReactDOM.render(
