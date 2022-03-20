1a7ab38303f4a22524a5bf2f10c00b09980f8317
zouhang <1102639563@qq.com>
2022-03-20 19:05:59 +0800

    fix: fix filter bug

--- src/pages/voting/CardSection/index.tsx
+++ src/pages/voting/CardSection/index.tsx
@@ -1,7 +1,7 @@
 import { CheckCircleTwoTone, ClockCircleFilled } from '@ant-design/icons'
 import Tag from 'components/Tag'
 import { NavLink } from 'react-router-dom'
-import React, { useContext } from 'react'
+import React, { useContext, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { shorten } from '@funcblock/dapp-sdk'
 import { useEthers } from '@usedapp/core'
@@ -27,36 +27,39 @@ export default function CardSection() {
 function Card({ item }) {
   const { t } = useTranslation()
   const info = useGovDetailInfo(item.proposalId)
-  const { state, voteEnd, voteStart, totalReward, proposer } = info
+  const { state, voteStart, proposer } = info
   const { status, related, timeRanges } = useContext(GovInfoFilterContext)
   const { account } = useEthers()
 
+  const show = useMemo(() => {
+    const statusShow = status === 'all' || status === state
+    const timeShow = !timeRanges.length || (moment.unix(voteStart).isAfter(timeRanges?.[0]) && moment.unix(voteStart).isBefore(timeRanges?.[1]))
+    const releatedShow = !related || account === proposer
+
+    return statusShow && timeShow && releatedShow
+  }, [status, state, timeRanges, voteStart, related, account, proposer])
+
+
   return (
-    <>
-      {
-        (status === 'all' || status === state)
-        && (!related || (proposer === account))
-        && (!timeRanges?.length || (moment.unix(voteStart).isAfter(timeRanges?.[0]) && moment.unix(voteStart).isBefore(timeRanges?.[1])))
-        &&
-        (<NavLink to={`/voting/detail/${item.proposalId}`} className="w-full mb-10 flex flex-col p-6 md:p-10 shadow rounded-lg">
-          <div className="flex flex-col md:flex-row-reverse md:mb-2">
-            <Tag type={state} className="min-w-16 h-7" />
-            <div className="mb-2 mt-4 text-base leading-6 font-medium text-yellow md:flex-1 md:mt-0 md:mb-0">
-              {t('author')}: {shorten(item.proposer)}
-            </div>
-          </div>
-          <div className="font-bold text-xl leading-6 break-words mb-2 text-light-black">
-            {item.description.slice(0, 20)}
-          </div>
-          <div className="break-words text-sm leading-5 mb-12 md:mb-9 text-dark-gray">
-            {item.description}
-          </div>
-          <div className="mb-2 flex flex-col md:flex-row md:items-center">
-            {getVoteStatusDesc(t, info)}
-          </div>
-        </NavLink>)
-      }
-    </>
+    <NavLink to={`/voting/detail/${item.proposalId}`} className={`w-full mb-10 flex flex-col p-6 md:p-10 shadow rounded-lg ${show ? 'block' : 'hidden'}`}>
+      <div className="flex flex-col md:flex-row-reverse md:mb-2">
+        <Tag type={state} className="min-w-16 h-7" />
+        <div className="mb-2 mt-4 text-base leading-6 font-medium text-yellow md:flex-1 md:mt-0 md:mb-0">
+          {t('author')}: {shorten(item.proposer)}
+        </div>
+      </div>
+      <div className="font-bold text-xl leading-6 break-words mb-2 text-light-black">
+        {item.description.slice(0, 20)}
+      </div>
+      <div className="break-words text-sm leading-5 mb-12 md:mb-9 text-dark-gray">
+        {item.description}
+      </div>
+      <div className="mb-2 flex flex-col md:flex-row md:items-center">
+        {
+          getVoteStatusDesc(t, info)
+        }
+      </div>
+    </NavLink>
   )
 }
 
