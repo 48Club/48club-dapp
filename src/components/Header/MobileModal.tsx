b7421fc69d1223606bdbcfdbebe00ef013cc695e
干净 <54979375+du5@users.noreply.github.com>
2023-12-19 12:47:24 +0800

    增加移动端入口

--- src/components/Header/MobileModal.tsx
+++ src/components/Header/MobileModal.tsx
@@ -74,6 +74,9 @@ export default function MobileModal(props: { open: boolean, oncancel: () => void
             <Link className="text-black opacity-75 hover:text-primary" to={'/pool'}>{t('nav-farm')}</Link>
           </Menu.Item>
           <Menu.Item>
+            <Link className="text-black opacity-75 hover:text-primary" to={'/committee'}>{t('inscriptions')}</Link>
+          </Menu.Item>
+          <Menu.Item>
             <Link className="text-black opacity-75 hover:text-primary" to={'/committee'}>{t('app_header_menu_committee_title')}</Link>
           </Menu.Item>
           <Menu.Item>
