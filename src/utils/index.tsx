806df61a4b0e68cfdf819b9c3606ea3178758dc0
lolowoo <153495021+lolowoo@users.noreply.github.com>
2024-01-14 01:16:08 +0800

    fix decimals

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,10 +1,12 @@
+import { ethers } from "ethers";
 
 
 export const decimalsToStr = (num: number, decimals: number) => {
-    return num / Math.pow(10, decimals);
+    if(num === undefined || decimals === undefined) return 0;
+    return +ethers.utils.formatUnits(num.toString(), decimals).toString()
 }
 
 
 export const strToDecimals = (num: number, decimals: number) => {
-    return num * Math.pow(10, decimals);
+    return +ethers.utils.parseUnits(num.toString(), decimals).toString()
 }
