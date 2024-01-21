7d714bd967f0efaad0860e9e6e16ed1c063ee9ba
kevin <1105626801@qq.com>
2024-01-21 18:25:10 +0800

    feat: add  loading

--- src/components/Modal/WalletModal/index.tsx
+++ src/components/Modal/WalletModal/index.tsx
@@ -7,23 +7,53 @@ import walletConnect from '@/assets/images/wallet/walletConnect.svg'
 import { styled } from 'styled-components'
 import { CHAIN_ID_HEX } from '@/constants/env'
 import { useEthers } from '@usedapp/core'
+import { LoadingOutlined } from '@ant-design/icons'
+const WalletContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  padding: 4px 12px 20px;
+`
+
+const WalletTitle = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  padding: 8px 8px;
+  > img {
+    cursor: pointer;
+  }
+`
+
 const WalletItem = styled.div`
   display: flex;
-  align-items: center;
+  justify-content: space-between;
   cursor: pointer;
   font-size: 16px;
   font-weight: 500;
-  padding: 12px 0px;
+  padding: 12px 8px 12px;
   border-radius: 12px;
+  border: 1px solid rgb(234, 234, 234);
   &:hover {
     background: rgba(34, 34, 34, 0.07);
   }
 `
 
+const WalletList = [
+  {
+    type: 'metamask',
+    src: metamask,
+  },
+  {
+    type: 'walletConnectV2',
+    src: walletConnect,
+  },
+]
+
 export default function WalletModal() {
   const modalOpen = useModalOpen(ApplicationModal.WALLET)
-  const closeModals = useCloseModals()
-  const { activateBrowserWallet } = useEthers()
+  const closeGlobalModal = useCloseModals()
+  const { activateBrowserWallet, account } = useEthers()
+  const [isActive, setActive] = useState('')
 
   const activate = useCallback(
     async (type: string) => {
@@ -37,32 +67,51 @@ export default function WalletModal() {
       } catch (e) {
         console.error(e)
       }
+      setActive(type)
       activateBrowserWallet({ type })
-      closeModals()
     },
     [activateBrowserWallet]
   )
+  const closeModal = () => {
+    closeGlobalModal()
+    setActive('')
+  }
+
+  useEffect(() => {
+    if (account) {
+      setActive('')
+      closeModal()
+    }
+  }, [account])
 
   return (
     <Modal
       open={modalOpen}
-      onCancel={closeModals}
+      onCancel={closeModal}
       transitionName=""
       centered
-      destroyOnClose
+      mask={false}
       footer={null}
       width={320}
       closeIcon={null}
+      zIndex={88}
     >
-      <div className="p-[20px] pr-[12px] pl-[12px]">
-        <WalletItem onClick={() => activate('metamask')}>
-          <img src={metamask} alt="" className="mr-[8px] rounded-[12px]" />
-          MetaMask
-        </WalletItem>
-        <WalletItem onClick={() => activate('walletConnectV2')}>
-          <img src={walletConnect} alt="" className="mr-[8px] rounded-[12px]" style={{}} /> WalletConnect
-        </WalletItem>
-      </div>
+      <WalletTitle>
+        <img src="/static/close.svg" alt="" onClick={closeModal} />
+      </WalletTitle>
+      <WalletContainer>
+        {WalletList.map((idx, i) => {
+          return (
+            <WalletItem onClick={() => activate(idx.type)} key={i + 'WalletItem'}>
+              <div className="flex items-center">
+                <img src={idx.src} alt="" className="mr-[8px] rounded-[12px]" />
+                {idx.type}
+              </div>
+              {isActive === idx.type && <LoadingOutlined />}
+            </WalletItem>
+          )
+        })}
+      </WalletContainer>
     </Modal>
   )
 }
