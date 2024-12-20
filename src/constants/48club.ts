e6e5522d223348bca0f7a14060e7f131471e582b
48ClubIan <sirian@48.club>
2024-12-20 12:30:40 +0800

    add tokenpocket logo

--- src/constants/48club.ts
+++ src/constants/48club.ts
@@ -53,20 +53,28 @@ export const MEDIUM_URL = 'https://medium.com/48-club-publish'
 
 export const PARTNER_URLS = [
   '/static/logo-partner-binance-smart-chain.svg',
+
   '/static/logo-partner-pancake.png',
   '/static/logo-partner-four.png',
+
   '/static/logo-partner-mathwallet.svg',
+  '/static/logo-partner-tokenpocket.svg',
+
   '/static/logo-partner-tenarmor.png',
   '/static/logo-partner-hashdit.jpg',
   '/static/logo-partner-rescuebox.jpg',
+
+  '/static/logo-partner-dodo.svg',
+  '/static/logo-partner-kava.svg',
+
   '/static/logo-partner-12.jpeg',
   '/static/logo-partner-realsatoshi.png',
-  '/static/logo-partner-dodo.svg',
-  '/static/logo-partner-defi-labs.svg',
-  '/static/logo-partner-alpaca.png',
-  '/static/logo-partner-roostoo.svg',
-  '/static/logo-hellodex.svg',
-  '/static/logo-partner-kava.svg',
-  '/static/logo-partner-bitfill.png',
   '/static/logo-partner-mayor-capital.svg',
+  '/static/logo-partner-defi-labs.svg',
+
+  '/static/logo-partner-roostoo.svg',
+
+  '/static/logo-partner-hellodex.svg',
+  '/static/logo-partner-alpaca.png',
+  '/static/logo-partner-bitfill.png',
 ]
