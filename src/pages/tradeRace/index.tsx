421e9091f466445efb40e55e69b44b8ee23a08fb
干净 <54979375+du5@users.noreply.github.com>
2025-07-01 00:22:01 +0800

    调整手续费计算比例以提高准确性

--- src/pages/tradeRace/index.tsx
+++ src/pages/tradeRace/index.tsx
@@ -77,8 +77,9 @@ export default function TradeRacePage() {
       if (!fee?.usdt_amount) {
         amount = '0'
       } else {
-        const totalFeeKoge = +fee.koge_amount * 0.648
-        const totalFee = +fee?.usdt_amount * 0.648
+          const ratio = 0.648 * 2
+          const totalFeeKoge = +fee.koge_amount * ratio
+          const totalFee = +fee?.usdt_amount * ratio
         const reward = (totalFee / total).toFixed(2)
         const rewardKoge = (totalFeeKoge / total).toFixed(4)
         if (data.rank === 0) {
